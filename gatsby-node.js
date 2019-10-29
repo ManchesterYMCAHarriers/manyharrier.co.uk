1cce2bbc4d6344e2cf9cc6176bc2baef3cf73697
Chris March <Chris.March@tfgm.com>
2019-10-29 22:55:59 +0000

    Use Stripe as source of membership options on join form

--- gatsby-node.js
+++ gatsby-node.js
@@ -154,7 +154,6 @@ exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions
 
   deletePage(page)
-  // You can access the variable "house" in your page queries now
   createPage({
     ...page,
     context: {
