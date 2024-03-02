e563bf84968579e04cc753de767b0c3744e12223
Chris March <Chris.March@tfgm.com>
2024-03-02 15:18:18 +0000

    Revert "Frog Graham"
    This reverts commit 1fd744cb0b90913a020ad12ceb087d4f38a3dbaa.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -259,6 +259,9 @@ export default class CheckoutIndex extends React.Component {
                     <option value="I am promoting an event">
                       I am promoting an event
                     </option>
+                    <option value="I need to contact the Covid Coordinator">I
+                      need to contact the Covid Coordinator
+                    </option>
                     <option value="Something else">Something else</option>
                   </FieldsetSelect>
 
