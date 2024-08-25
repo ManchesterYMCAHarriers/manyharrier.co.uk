1eb59e64569bdd0bf12213410265b31888d0b779
Nathaniel <nstanton1@sheffield.ac.uk>
2024-08-26 00:05:18 +0100

    Fixed a bug.

--- src/components/CommitteePanel.js
+++ src/components/CommitteePanel.js
@@ -4,19 +4,10 @@ import Img from 'gatsby-image'
 
 export const CommitteePanel = ({ name, role, description, keySkill, favouriteRace, image}) => (
     <div className="flex flex-col w-64 bg-white-manyharrier">
-        <div className="relative w-full h-96">
-            <Img 
-                fixed={image}
-                alt={'Photo of ' + name} 
-                style={{
-                    position: 'absolute',
-                    height: '100%',
-                    width: '100%',
-                    inset: 0,
-                    opacity: 0.1,
-                }}
-                className="z-0" 
-            />
+        <div className="relative h-96">
+            <div className='absolute inset-0 h-full w-full z-0 opacity-10'>
+                <Img fixed={image} alt={'Photo of ' + name}/>
+            </div>
             <div className="absolute inset-0 z-10 content-center m-3 text-base text-black">
                 <div className='content' dangerouslySetInnerHTML={{__html: description}}/>
                 <p className="font-semibold text-red-manyharrier"><br/>Key Skill:</p>
@@ -24,18 +15,9 @@ export const CommitteePanel = ({ name, role, description, keySkill, favouriteRac
                 <p className="font-semibold text-red-manyharrier">Favourite Race:</p>
                 <p>{favouriteRace}</p>
             </div>
-            <Img 
-                fixed={image} 
-                alt={'Photo of ' + name} 
-                style={{
-                    position: 'absolute',
-                    height: '100%',
-                    width: '100%',
-                    inset: 0,
-                    opacity: 1,
-                }}
-                className="z-20 hover:opacity-0 duration-300"
-            />
+            <div className='absolute inset-0 h-full w-full z-20 hover:opacity-0 duration-300'>
+                <Img fixed={image} alt={'Photo of ' + name}/>
+            </div>
         </div>
         <div className="text-center text-base text-black font-semibold self-center py-1">{name}</div>
         <div className="text-center text-base font-semibold self-center text-red-manyharrier mb-2">{role}</div>
