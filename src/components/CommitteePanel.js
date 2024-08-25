e905ff9c1acaaf4ffe56fe3d3e65f0512e3ba9d3
Nathaniel <nstanton1@sheffield.ac.uk>
2024-08-25 22:53:15 +0100

    Fixed a bug.

--- src/components/CommitteePanel.js
+++ src/components/CommitteePanel.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
-export const CommitteePanel = ({ name, role, description, keySkill, favouriteRace, image}) => {
+export const CommitteePanel = ({ name, role, description, keySkill, favouriteRace, image}) => (
     <div className="flex flex-col w-64 bg-white-manyharrier">
         <div className="relative h-96">
         <Img fixed={image} alt={'Photo of ' + name} className={`absolute inset-0 h-full w-full z-0 opacity-10`} />
@@ -17,7 +17,7 @@ export const CommitteePanel = ({ name, role, description, keySkill, favouriteRac
         </div>
         <div className="text-center text-base text-black font-semibold self-center py-1"><span>{name}</span> <span className="text-red-600">{`&nbsp&nbsp` + role}</span></div>
     </div>
-}
+)
 
 CommitteePanel.propTypes = {
     name: PropTypes.string.isRequired,
