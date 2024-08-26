d974fa32676111fb405e8d0115c5b14f486559d9
Nathaniel <nstanton1@sheffield.ac.uk>
2024-08-26 01:02:00 +0100

    Fixed a bug.

--- src/components/CommitteePanel.js
+++ src/components/CommitteePanel.js
@@ -4,23 +4,17 @@ import Img from 'gatsby-image'
 
 export const CommitteePanel = ({ name, role, description, keySkill, favouriteRace, image}) => (
     <div className="flex flex-col w-full bg-white-manyharrier">
-        <div className="flex flex-none relative w-64 h-96">
-            <div className='absolute inset-0 h-full w-full z-0 opacity-10'>
-                <Img fixed={image} alt={'Photo of ' + name}/>
-            </div>
-            <div className="flex absolute inset-0 z-10 content-center m-3 text-base text-black">
+        <div className='flex flex-col md:flex-row w-full'>
+            <Img fixed={image} alt={'Photo of ' + name}/>
+            <div className="flex content-center m-3 text-base text-black">
                 <div className='content' dangerouslySetInnerHTML={{__html: description}}/>
-                <p className="font-semibold text-red-manyharrier"><br/>Key Skill:</p>
+                <p className="font-semibold text-red-manyharrier">Key Skill:</p>
                 <p>{keySkill}</p>
                 <p className="font-semibold text-red-manyharrier">Favourite Race:</p>
                 <p>{favouriteRace}</p>
             </div>
-            <div className='flex absolute inset-0 h-full w-full z-20 hover:opacity-0 duration-300'>
-                <Img fixed={image} alt={'Photo of ' + name}/>
-            </div>
         </div>
-        <div className="text-center text-base text-black font-semibold self-center py-1">{name}</div>
-        <div className="text-center text-base font-semibold self-center text-red-manyharrier mb-2">{role}</div>
+        <div className="text-center text-base text-black font-semibold self-center py-1">{name + "\t\t" + role}</div>
     </div>
 )
 
